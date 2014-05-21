f441d9ef321df4f2375a97b404d972615a9b0cb8
Kevin <ktripp1@gmail.com>
2014-05-21 10:39:02 -0400

    removed debugging code

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,10 @@
 var map;
 
 $('.select-petersfield').on('click',function(){
-	console.log('************************************')
 	$('.chelsea').slideToggle();
-	$('.petersfield').slideToggle();
-	getMap('362 2nd Ave,New York, NY','map-canvas','Pushcart Coffee Peters field');
-	console.log('petersfield complete');
+	$('.petersfield').slideToggle()
+	getMap('362 2nd Ave,New York, NY','map-petersfield','Pushcart Coffee Peters field');
+	//fixes and issue where the map isn't displayed properly due to the div being hidden on load
 	lastCenter=map.getCenter(); 
 	google.maps.event.trigger(map, 'resize');
 	map.setCenter(lastCenter);
@@ -16,6 +15,10 @@ $('.select-petersfield').on('click',function(){
 $('.select-chelsea').on('click',function(){
 	$('.petersfield').slideToggle();
 	$('.chelsea').slideToggle();
+	//fixes and issue where the map isn't displayed properly due to the div being hidden on load
+	lastCenter=map.getCenter(); 
+	google.maps.event.trigger(map, 'resize');
+	map.setCenter(lastCenter);
 });
 
 $('#navList li a').on('click',function(e){
@@ -30,14 +33,12 @@ $('#navList li a').on('click',function(e){
 
 function getMap(address, elementId, nameOfLocation){
 	//key for google maps to work
-	var id = 'AIzaSyAqB0Oju84S-3pjqi6-qoNHDTvGLSTFL50';
+	var __KEYID__ = 'AIzaSyAqB0Oju84S-3pjqi6-qoNHDTvGLSTFL50';
 	//constructiing the url to be used later
 	var url = 'http://maps.googleapis.com/maps/api/geocode/json?address='
 		+ address
 		+ '&sensor=false&key'
-		+ id
-
-	console.log(url)
+		+ __KEYID__
 	//creating the variables to save the lat and long to after the ajax call
 	var lat = "";
 	var lng = "";
@@ -49,13 +50,7 @@ function getMap(address, elementId, nameOfLocation){
 		lat = data.results[0].geometry.location.lat;
 		lng = data.results[0].geometry.location.lng;
 
-		console.log(lat, lng)
-
 		var myLatlng = new google.maps.LatLng(lat, lng);
-		console.log(myLatlng)
-
-		console.log('created map variable' + nameOfLocation)
-
 		function initialize() {
 			//constructing the map options
 			var mapOptions = {
@@ -63,26 +58,22 @@ function getMap(address, elementId, nameOfLocation){
 			    center: myLatlng,
 			    scrollwheel: false
 			  };
-			  console.log('created map options'+ nameOfLocation)
 			  //constructing the map to be displayed
 			  map = new google.maps.Map(document.getElementById(elementId)
 			  	, mapOptions);
-			  console.log('created map'+ nameOfLocation)
 			//creating the marker for the business location
 			var marker = new google.maps.Marker({
       				position: myLatlng,
       				map: map,
      				title: nameOfLocation
   			});
-  			console.log('created marker'+ nameOfLocation)
-
 		}
 		//calling the map api to create and display the map
 		google.maps.event.addDomListener(window, 'load', initialize);
-		console.log('created and loaded the map'+ nameOfLocation)
   }});
 }
 //loading the chelsea map by defualt
 getMap('401+W+25th+St,+New+York,+NY,+10001','map-chelsea','Pushcart Coffee @ chelsea');
-getMap('362+2nd+Ave,New+York,+NY','map-petersfield','Pushcart Coffee Peters field');
+//loading the petersfield map by defualt
+getMap('362 2nd Ave,New York, NY','map-petersfield','Pushcart Coffee Peters field');
 
